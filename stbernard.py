3317756a936b7cb68c78e9205509c7ab50149ecc
incub4t0r <dchung0812@gmail.com>
2020-11-16 21:04:23 -0500

    Update with brewbanking system

--- stbernard.py
+++ stbernard.py
@@ -1,5 +1,5 @@
 #importing the discord module
-import discord, datetime, os, random
+import discord, datetime, os, random, json
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -11,11 +11,67 @@ file_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__fil
 
 bot = commands.Bot(command_prefix="!")
 
+# Prepares bot with balance system
 @bot.event
 async def on_ready():
     print('Logged in')
     print("Username: ",end='')
     print(bot.user.name)
+    global brewbucks
+    try:
+        with open(os.path.join(file_location, 'brewbucks.json')) as f:
+            brewbucks = json.load(f)
+    except:
+        print("Could not load brewbucks.json")
+        brewbucks = {}
+
+### BALANCE SYSTEM START ###
+@bot.command()
+async def balance(ctx):
+    id = str(ctx.message.author.id)
+    if id in brewbucks:
+        await ctx.send("You have {} brewbucks".format(brewbucks[id]))
+    else:
+        await ctx.send("You do not have an account")
+
+@bot.command()
+async def register(ctx):
+    id = str(ctx.message.author.id)
+    if id not in brewbucks:
+        brewbucks[id] = 100
+        await ctx.send("You are now registered")
+        _save()
+    else:
+        await ctx.send("You already have an account")
+
+@bot.command(pass_context=True)
+async def transfer(ctx, amount: int, other: discord.Member):
+    primary_id = str(ctx.message.author.id)
+    other_id = str(other.id)
+    if primary_id not in brewbucks:
+        await ctx.send("You do not have an account")
+    elif other_id not in brewbucks:
+        await ctx.send("The other party does not have an account")
+    elif brewbucks[primary_id] < amount:
+        await ctx.send("You cannot afford this transaction")
+    else:
+        brewbucks[primary_id] -= amount
+        brewbucks[other_id] += amount
+        await ctx.send("Transaction complete")
+    _save()
+
+def _save():
+    with open(os.path.join(file_location, 'brewbucks.json'), 'w+') as f:
+        json.dump(brewbucks, f)
+
+@bot.command()
+async def save(ctx):
+    _save()
+
+#https://stackoverflow.com/questions/55485988/discord-money-bot-keeping-user-ids-in-json-file-when-bot-restarts-it-creats-a
+### BALANCE SYSTEM END ###
+
+### COMMANDS START ###
 
 @bot.command(
     help = "Utilizes 1337 coding to determine correct response and latency",
@@ -52,16 +108,23 @@ async def countdown(ctx):
     final_string = "".join((msg_today, msg_remaining))
     emb = discord.Embed(title=None, description=final_string)
     await ctx.send(embed=emb)
-
-#@bot.command()
-#async def bonk(ctx,member_id:int):
-    #author = ctx.author
-#    member = ctx.guild.get_member(member_id)
-#    print (member)
-#    final_string = f"{ctx.author.mention} bonked {member.mention}"
-    #final_string = "{author} slapped {member}!".format(
-    #    author=ctx.author.mention, member=member.mention)
-#    await ctx.send(final_string)
+    
+@bot.command(
+    help = "Calculates using datetime the amount of time remaining until graduation",
+    brief = "Prints time until graduation '23")
+async def graduation(ctx):
+    dateLeave = datetime.datetime(2023, 5, 27)
+    today = datetime.datetime.now()
+    delta = dateLeave-today
+    date_split = str(delta).split()
+    date_small_split = date_split[2].split(":")
+    
+    msg_today = "Hello Brewdawg. \n\nToday is " + today.strftime("%A") + ".\n"
+    msg_remaining = "For the Class of 2023, there are `{days}` days, `{hours}` hours, `{minutes}` minutes, and `{seconds}` seconds remaining until graduation.".format(
+        days=date_split[0],hours=date_small_split[0],minutes=date_small_split[1],seconds=round(float(date_small_split[2])))
+    final_string = "".join((msg_today, msg_remaining))
+    emb = discord.Embed(title=None, description=final_string)
+    await ctx.send(embed=emb)
 
 @bot.command(
     help="Send someone to horny jail",
@@ -90,5 +153,12 @@ async def godawgs(ctx):
         final_string += line
     await ctx.send(f"```{final_string}```")
 
-
 bot.run(TOKEN)
+
+#https://stackoverflow.com/questions/63625197/how-to-make-a-currency-system-in-discord-py
+
+#TODO
+# add a brewbuck system
+# add exception handling
+# add a bonk tracker
+# add a !source to give a link to the github it is hosted on
